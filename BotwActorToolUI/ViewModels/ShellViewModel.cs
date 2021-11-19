1e07c68ec730ffa579d414b53283acfdf3eb1472
ArchLeaders <archparliament@gmail.com>
2021-11-18 21:31:44 -0800

    Add project files.

---
+++ BotwActorToolUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotwActorToolUI.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+
+    }
+}
