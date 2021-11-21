fb104a8c09814dd02a499d9c9dfdd724216a0452
ArchLeaders <archparliament@gmail.com>
2021-11-20 19:26:34 -0800

    Partialy workig tabs. Basic menu implementation.

---
+++ BotwActorToolUI/AppBootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using BotwActorToolUI.ViewModels;
+using Stylet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BotwActorToolUI
+{
+    public class AppBootstrapper : Bootstrapper<ShellViewModel> {}
+}
