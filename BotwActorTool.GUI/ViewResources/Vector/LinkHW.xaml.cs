3d42737cb3f638d2dc1205be2ef3b1ad51e0f38a
ArchLeaders <archparliament@gmail.com>
2022-06-18 01:24:12 -0700

    Create theme editor; Add LinkHW vector graphic

---
+++ BotwActorTool.GUI/ViewResources/Vector/LinkHW.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace BotwActorTool.GUI.ViewResources.Vector
+{
+    /// <summary>
+    /// Interaction logic for Botw2.xaml
+    /// </summary>
+    public partial class LinkHW : UserControl
+    {
+        public LinkHW()
+        {
+            InitializeComponent();
+        }
+
+        public void SetFill(Color color) => vector.Fill = new SolidColorBrush(color);
+    }
+}
