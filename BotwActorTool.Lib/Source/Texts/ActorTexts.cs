ff5552fa2426174570efc9efb767e1f7eeed5b61
GingerAvalanche <chodness@gmail.com>
2022-06-20 01:43:36 -0700

    Rename ActorText variables for consistency

--- BotwActorTool.Lib/Source/Texts/ActorTexts.cs
+++ BotwActorTool.Lib/Source/Texts/ActorTexts.cs
@@ -5,9 +5,10 @@ namespace BotwActorTool.Lib.Texts
     class ActorTexts
     {
         public string ActorName { private get; set; }
-        private readonly string OrigActorName;
-        private readonly string Profile;
-        private readonly Dictionary<string, MsbtEntry> Texts = new()
+        public Dictionary<string, MsbtEntry> Texts { get => texts; }
+        private readonly string orig_actor_name;
+        private readonly string profile;
+        private readonly Dictionary<string, MsbtEntry> texts = new()
         {
             { "BaseName", new("", "") },
             { "Name", new("", "") },
@@ -18,43 +19,41 @@ namespace BotwActorTool.Lib.Texts
         public ActorTexts(string pack, string profile)
         {
             ActorName = Path.GetFileNameWithoutExtension(pack);
-            OrigActorName = ActorName;
-            Profile = profile;
+            orig_actor_name = ActorName;
+            this.profile = profile;
             string lang = Config.Lang;
-            MSBT msbt = new(new MemoryStream(Util.GetFileAnywhere(Util.GetModRoot(pack), $"Pack/Bootup_{lang}.pack//Message/Msg_{lang}.product.ssarc//ActorType/{Profile}.msbt") ?? Array.Empty<byte>()));
+            MSBT msbt = new(new MemoryStream(Util.GetFileAnywhere(Util.GetModRoot(pack), $"Pack/Bootup_{lang}.pack//Message/Msg_{lang}.product.ssarc//ActorType/{this.profile}.msbt") ?? Array.Empty<byte>()));
             Dictionary<string, MsbtEntry> temp = msbt.GetTexts();
-            foreach (KeyValuePair<string, MsbtEntry> kvp in Texts) {
+            foreach (KeyValuePair<string, MsbtEntry> kvp in texts) {
                 if (temp.ContainsKey($"{ActorName}_{kvp.Key}")) {
-                    Texts[kvp.Key] = temp[$"{ActorName}_{kvp.Key}"];
+                    texts[kvp.Key] = temp[$"{ActorName}_{kvp.Key}"];
                 }
             }
         }
 
-        public MsbtEntry this[string str] => Texts[str];
-
         public void DeleteActor(string modRoot)
         {
             string lang = Config.Lang;
-            MSBT msbt = new(new MemoryStream(Util.GetFileAnywhere(modRoot, $"Pack/Bootup_{lang}.pack//Message/Msg_{lang}.product.ssarc//ActorType/{Profile}.msbt") ?? Array.Empty<byte>()));
+            MSBT msbt = new(new MemoryStream(Util.GetFileAnywhere(modRoot, $"Pack/Bootup_{lang}.pack//Message/Msg_{lang}.product.ssarc//ActorType/{profile}.msbt") ?? Array.Empty<byte>()));
             Dictionary<string, MsbtEntry> AllTexts = msbt.GetTexts();
-            AllTexts.Remove($"{OrigActorName}_BaseName");
-            AllTexts.Remove($"{OrigActorName}_Name");
-            AllTexts.Remove($"{OrigActorName}_Desc");
-            AllTexts.Remove($"{OrigActorName}_PictureBook");
+            AllTexts.Remove($"{orig_actor_name}_BaseName");
+            AllTexts.Remove($"{orig_actor_name}_Name");
+            AllTexts.Remove($"{orig_actor_name}_Desc");
+            AllTexts.Remove($"{orig_actor_name}_PictureBook");
             msbt.SetTexts(AllTexts);
-            Util.InjectFile(modRoot, $"Pack/Bootup_{lang}.pack//Message/Msg_{lang}.product.ssarc//ActorType/{Profile}.msbt", msbt.Write());
+            Util.InjectFile(modRoot, $"Pack/Bootup_{lang}.pack//Message/Msg_{lang}.product.ssarc//ActorType/{profile}.msbt", msbt.Write());
         }
 
         public void Write(string modRoot)
         {
             string lang = Config.Lang;
-            MSBT msbt = new(new MemoryStream(Util.GetFileAnywhere(modRoot, $"Pack/Bootup_{lang}.pack//Message/Msg_{lang}.product.ssarc//ActorType/{Profile}.msbt") ?? Array.Empty<byte>()));
+            MSBT msbt = new(new MemoryStream(Util.GetFileAnywhere(modRoot, $"Pack/Bootup_{lang}.pack//Message/Msg_{lang}.product.ssarc//ActorType/{profile}.msbt") ?? Array.Empty<byte>()));
             Dictionary<string, MsbtEntry> AllTexts = msbt.GetTexts();
-            foreach (KeyValuePair<string, MsbtEntry> kvp in Texts) {
+            foreach (KeyValuePair<string, MsbtEntry> kvp in texts) {
                 AllTexts[$"{ActorName}_{kvp.Key}"] = kvp.Value;
             }
             msbt.SetTexts(AllTexts);
-            Util.InjectFile(modRoot, $"Pack/Bootup_{lang}.pack//Message/Msg_{lang}.product.ssarc//ActorType/{Profile}.msbt", msbt.Write());
+            Util.InjectFile(modRoot, $"Pack/Bootup_{lang}.pack//Message/Msg_{lang}.product.ssarc//ActorType/{profile}.msbt", msbt.Write());
         }
     }
 }
