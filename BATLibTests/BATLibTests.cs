da78e9401c3a07ea5021a153c2ac2afcefb8538c
GingerAvalanche <chodness@gmail.com>
2022-06-19 19:53:24 -0700

    Update tests

--- BATLibTests/BATLibTests.cs
+++ BATLibTests/BATLibTests.cs
@@ -1,4 +1,6 @@
 using BotwActorTool.Lib;
+using Nintendo.Aamp;
+using System.Diagnostics;
 
 namespace BATLibTests
 {
@@ -10,6 +12,12 @@ namespace BATLibTests
         {
             Assert.AreEqual("C:/mod/content", Util.GetModRoot("C:/mod/content/Actor/ActorInfo.product.sbyml"));
             Assert.AreEqual("C:/mod/romfs", Util.GetModRoot("C:/mod/romfs/Actor/ActorInfo.product.sbyml"));
+        }
+        [TestMethod]
+        public void ReadAamp()
+        {
+            AampFile aamp = new(@"E:\Users\chodn\Documents\CemuShit\Remodels\Zelda\Byleth\mod content_unbuilt\content\Actor\ActorLink\Armor_001_G_Upper.bxml");
+            Trace.WriteLine(aamp.ToYml());
         }
     }
 }
