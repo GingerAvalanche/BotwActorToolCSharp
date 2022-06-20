f94a62070b9615af7c3edf84e1967d3e728bbfc9
GingerAvalanche <chodness@gmail.com>
2022-06-20 05:09:41 -0700

    Placeholder ActorInfo

---
+++ BotwActorTool.Lib/Source/Info/ActorInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotwActorTool.Lib.Info
+{
+    internal class ActorInfo
+    {
+        private Actor? actor;
+        private FarActor? far;
+
+        public ActorInfo(Actor actor)
+        {
+            this.actor = actor;
+        }
+        public ActorInfo(FarActor far)
+        {
+            this.far = far;
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
