ce9b66844945e4d3da3e33f36b33ea39cb45a185
GingerAvalanche <chodness@gmail.com>
2022-06-20 01:44:31 -0700

    Convert Data files to embedded resources

---
+++ BotwActorTool.Lib/Source/Resource.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Text.Json;
+
+namespace BotwActorTool.Lib
+{
+    public class Resource
+    {
+        public Dictionary<string, dynamic> Data { get; set; } = new();
+        public Resource(string resourceName)
+        {
+            Assembly assembly = Assembly.GetCallingAssembly();
+            using (Stream? stream = assembly.GetManifestResourceStream("BotwActorTool.Lib." + resourceName))
+            {
+                if (stream != null)
+                {
+                    Data = (Dictionary<string, dynamic>)JsonSerializer.Deserialize(stream, typeof(Dictionary<string, dynamic>))!;
+                }
+            }
+        }
+    }
+}
