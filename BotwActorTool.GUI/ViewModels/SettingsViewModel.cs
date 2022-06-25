6d32eefb00363229a0c6f36c7dfcd28e02f2c260
ArchLeaders <archparliament@gmail.com>
2022-06-24 21:50:45 -0700

    Fix and simplify path validation

--- BotwActorTool.GUI/ViewModels/SettingsViewModel.cs
+++ BotwActorTool.GUI/ViewModels/SettingsViewModel.cs
@@ -9,6 +9,19 @@ namespace BotwActorTool.GUI.ViewModels
 {
     public class SettingsViewModel : Screen
     {
+        private Brush ValidatePath(string path, string mode)
+        {
+            if (string.IsNullOrEmpty(path)) {
+                return _default;
+            }
+            else if (Config.ValidateDir(path, mode)) {
+                return _valid;
+            }
+            else {
+                return _invalid;
+            }
+        }
+
         private static readonly Brush _default = new SolidColorBrush(SysTheme.ITheme.PrimaryMid.Color);
         private static readonly Brush _valid = "#0CED5F".ToBrush();
         private static readonly Brush _invalid = "#ED2A64".ToBrush();
@@ -19,16 +32,7 @@ namespace BotwActorTool.GUI.ViewModels
             get => _baseGame;
             set {
                 SetAndNotify(ref _baseGame, value);
-                if (_baseGame == "")
-                {
-                    BaseGameBrush = _default;
-                }
-                else if (Config.ValidateGameDir(value)) {
-                    BaseGameBrush = _valid;
-                }
-                else {
-                    BaseGameBrush = _invalid;
-                }
+                BaseGameBrush = ValidatePath(value, "base");
             }
 
         }
@@ -38,16 +42,7 @@ namespace BotwActorTool.GUI.ViewModels
             get => _update;
             set {
                 SetAndNotify(ref _update, value);
-                if (_update == "")
-                {
-                    UpdateBrush = _default;
-                }
-                else if (Config.ValidateUpdateDir(value)) {
-                    UpdateBrush = _valid;
-                }
-                else {
-                    UpdateBrush = _invalid;
-                }
+                UpdateBrush = ValidatePath(value, "update");
             }
         }
 
@@ -56,61 +51,26 @@ namespace BotwActorTool.GUI.ViewModels
             get => _dlc;
             set {
                 SetAndNotify(ref _dlc, value);
-                if (_dlc == "")
-                {
-                    DlcBrush = _default;
-                }
-                else if (Config.ValidateDlcDir(value)) {
-                    DlcBrush = _valid;
-                }
-                else {
-                    DlcBrush = _invalid;
-                }
+                DlcBrush = ValidatePath(value, "dlc");
             }
         }
 
         private string _baseGameNx = "";
-        public string BaseGameNx
-        {
+        public string BaseGameNx {
             get => _baseGameNx;
-            set
-            {
+            set {
                 SetAndNotify(ref _baseGameNx, value);
-                if (_baseGameNx == "")
-                {
-                    BaseGameNxBrush = _default;
-                }
-                else if (Config.ValidateGameDir(value) && Config.ValidateUpdateDir(value))
-                {
-                    BaseGameNxBrush = _valid;
-                }
-                else
-                {
-                    BaseGameNxBrush = _invalid;
-                }
+                BaseGameNxBrush = ValidatePath(value, "baseNx");
             }
 
         }
 
         private string _dlcNx = "";
-        public string DlcNx
-        {
+        public string DlcNx {
             get => _dlcNx;
-            set
-            {
+            set {
                 SetAndNotify(ref _dlcNx, value);
-                if (_dlcNx == "")
-                {
-                    DlcNxBrush = _default;
-                }
-                else if (Config.ValidateDlcDir(value))
-                {
-                    DlcNxBrush = _valid;
-                }
-                else
-                {
-                    DlcNxBrush = _invalid;
-                }
+                DlcNxBrush = ValidatePath(value, "dlcNx");
             }
         }
 
@@ -133,15 +93,13 @@ namespace BotwActorTool.GUI.ViewModels
         }
 
         private Brush _baseGameNxBrush = _default;
-        public Brush BaseGameNxBrush
-        {
+        public Brush BaseGameNxBrush {
             get => _baseGameNxBrush;
             set => SetAndNotify(ref _baseGameNxBrush, value);
         }
 
         private Brush _dlcNxBrush = _default;
-        public Brush DlcNxBrush
-        {
+        public Brush DlcNxBrush {
             get => _dlcNxBrush;
             set => SetAndNotify(ref _dlcNxBrush, value);
         }
@@ -150,12 +108,6 @@ namespace BotwActorTool.GUI.ViewModels
         public string Lang {
             get => _lang;
             set => SetAndNotify(ref _lang, value);
-        }
-
-        private Visibility _showUpdate = Visibility.Visible;
-        public Visibility ShowUpdate {
-            get => _showUpdate;
-            set => SetAndNotify(ref _showUpdate, value);
         }
 
         private BindableCollection<string> _themes = new();
@@ -183,38 +135,48 @@ namespace BotwActorTool.GUI.ViewModels
         public void Save()
         {
             if (BaseGameBrush == _invalid) {
-                _shellViewModel.WindowManager.Show("The Wii U game path is invalid.\nPlease correct or delete it before saving.", "Error");
+                _shellViewModel.WindowManager.Show("The WiiU game path is invalid.\nPlease correct or delete it before saving.", "Error");
                 return;
             }
 
             if (UpdateBrush == _invalid) {
-                _shellViewModel.WindowManager.Show("The Wii U update path is invalid.\nPlease correct or delete it before saving.", "Error");
+                _shellViewModel.WindowManager.Show("The WiiU update path is invalid.\nPlease correct or delete it before saving.", "Error");
                 return;
             }
 
-            if (DlcBrush == _invalid)
-            {
-                _shellViewModel.WindowManager.Show("The Wii U DLC path is invalid.\nPlease correct or delete it before saving.", "Error");
+            if (DlcBrush == _invalid) {
+                _shellViewModel.WindowManager.Show("The WiiU DLC path is invalid.\nPlease correct or delete it before saving.", "Error");
                 return;
             }
 
-            if (BaseGameNxBrush == _invalid)
-            {
+            if (BaseGameNxBrush == _invalid && BaseGameBrush == _invalid) {
                 _shellViewModel.WindowManager.Show("The Switch game/update path is invalid.\nPlease correct or delete it before saving.", "Error");
                 return;
             }
 
-            if (DlcNxBrush == _invalid)
-            {
+            if (DlcNxBrush == _invalid) {
                 _shellViewModel.WindowManager.Show("The Switch DLC path is invalid.\nPlease correct or delete it before saving.", "Error");
+                return;
+            }
+
+            if (BaseGameBrush == _default && UpdateBrush == _default && BaseGameNxBrush == _default) {
+                _shellViewModel.WindowManager.Show("No game path has been set for Switch or WiiU.\nPlease set one of them before saving.", "Error");
+                return;
+            }
+
+            if (BaseGameBrush == _valid && UpdateBrush == _default ) {
+                _shellViewModel.WindowManager.Show("The WiiU update path has not been set.\nPlease set it before saving.", "Error");
                 return;
             }
 
             Config.GameDir = BaseGame;
             Config.UpdateDir = Update;
             Config.DlcDir = Dlc;
+            Config.GameDirNx = BaseGameNx;
+            Config.DlcDirNx = DlcNx;
             Config.Theme = Theme;
             Config.Lang = Lang;
+
             Config.Save();
 
             Close();
@@ -234,12 +196,10 @@ namespace BotwActorTool.GUI.ViewModels
                 else if (mode == "dlc") {
                     Dlc = browse.SelectedPath;
                 }
-                else if (mode == "baseNx")
-                {
+                else if (mode == "baseNx") {
                     BaseGameNx = browse.SelectedPath;
                 }
-                else if (mode == "dlcNx")
-                {
+                else if (mode == "dlcNx") {
                     DlcNx = browse.SelectedPath;
                 }
             }
