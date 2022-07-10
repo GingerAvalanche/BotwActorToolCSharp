d85524474bdb06644bd8a98a6cbd348dac30fba5
ArchLeaders <archparliament@gmail.com>
2022-07-10 11:34:33 -0700

    Create frontend extensions

---
+++ BotwActorTool.GUI/Extensions/BrushExt.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotwActorTool.GUI.Extensions
+{
+    public static class BrushExt
+    {
+        public static Brush ToBrush(this string color) => (Brush)(new BrushConverter().ConvertFromString(color) ?? new());
+        public static Brush? GetBrush(this string brush)
+        {
+            if (Avalonia.Application.Current != null) {
+                Avalonia.Application.Current.Resources.TryGetResource(brush, out object? obj);
+                return obj as Brush;
+            }
+            else {
+                return null;
+            }
+        }
+    }
+}
---
+++ BotwActorTool.GUI/Extensions/ViewExt.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia.Controls;
+using BotwActorTool.GUI.ViewModels;
+using BotwActorTool.GUI.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BotwActorTool.GUI.Views.MessageBox;
+
+namespace BotwActorTool.GUI.Extensions
+{
+    public enum MessageBoxButtons { Ok, OkCancel, YesNo, YesNoCancel }
+    public enum MessageBoxResult { Cancel, No, Ok, Yes }
+
+    public static class ViewExt
+    {
+        public static Task<MessageBoxResult> ShowMessageBox(this Window parent, string text, string title, MessageBoxButtons buttons = MessageBoxButtons.Ok)
+            => Show(text, title, buttons, parent);
+    }
+}
