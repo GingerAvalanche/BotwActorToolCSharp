4d17b3d1341828b53b10163dc5f8c200621d9880
ArchLeaders <archparliament@gmail.com>
2022-07-11 14:45:21 -0700

    Dock helpers

--- BotwActorTool.GUI/Extensions/ViewExt.cs
+++ BotwActorTool.GUI/Extensions/ViewExt.cs
@@ -1,6 +1,9 @@
 ﻿using Avalonia.Controls;
 using BotwActorTool.GUI.ViewModels;
 using BotwActorTool.GUI.Views;
+using Dock.Model.Core;
+using Dock.Model.ReactiveUI.Controls;
+using Dock.Model.ReactiveUI.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +15,31 @@ namespace BotwActorTool.GUI.Extensions
 {
     public enum MessageBoxButtons { Ok, OkCancel, YesNo, YesNoCancel }
     public enum MessageBoxResult { Cancel, No, Ok, Yes }
-
+    public enum Formatting { None, Markdown }
     public static class ViewExt
     {
-        public static Task<MessageBoxResult> ShowMessageBox(this Window parent, string text, string title = "Warning", MessageBoxButtons buttons = MessageBoxButtons.Ok)
-            => Show(text, title, buttons, parent);
+        public static Task<MessageBoxResult> ShowMessageBox(this Window parent, string text, string title = "Warning", MessageBoxButtons buttons = MessageBoxButtons.Ok,
+            Formatting formatting = Formatting.None) => Show(text, title, buttons, parent, formatting);
+
+        public static void SetActive(this DockBase dock, string id)
+        {
+            foreach (var doc in dock.VisibleDockables!) {
+                if (doc.Id == id) {
+                    dock.ActiveDockable = doc;
+                    return;
+                }
+            }
+        }
+
+        public static IDockable? Get(this DockBase dock, string id)
+        {
+            foreach (var doc in dock.VisibleDockables!) {
+                if (doc.Id == id) {
+                    return doc;
+                }
+            }
+
+            return null;
+        }
     }
 }
