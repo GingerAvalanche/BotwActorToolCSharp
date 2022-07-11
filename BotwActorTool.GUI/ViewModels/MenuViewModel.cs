e6bc05fd227922ca471b4c18d29ddc5c6b683825
ArchLeaders <archparliament@gmail.com>
2022-07-11 14:46:36 -0700

    Menu commands

--- BotwActorTool.GUI/ViewModels/MenuViewModel.cs
+++ BotwActorTool.GUI/ViewModels/MenuViewModel.cs
@@ -1,7 +1,13 @@
-﻿using ReactiveUI;
+﻿using Avalonia.Controls;
+using BotwActorTool.GUI.Extensions;
+using BotwActorTool.Lib;
+using Dock.Model.ReactiveUI.Controls;
+using Material.Icons;
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,28 +19,84 @@ namespace BotwActorTool.GUI.ViewModels
         //
         // File
 
-        public void OpenVanillaActor()
+        public void OpenVanillaActor() => ToolDock.SetActive("VanillaFiles");
+
+        public async void OpenMod()
         {
-            // if (game paths are valid)
-            //    open in-app actor browser (from vanilla)
+            var result = await new OpenFolderDialog() {
+                Title = $"Select a mod 'content' folder"
+            }.ShowAsync(View);
+
+            if (result == null)
+                return;
+
+            bool found = true;
+            foreach (var ending in new string[] { "content", "romfs" }) {
+                found = result.EndsWith(ending);
+
+                if (Directory.Exists($"{result}\\{ending}".ToSystemPath())) {
+                    found = true;
+                    result = $"{result}\\{ending}";
+                }
+
+                if (found) {
+                    break;
+                }
+            }
+
+            if (!found) {
+                await View.ShowMessageBox("The selected folder is not a valid mod folder.", "Error");
+                OpenMod();
+                return;
+            }
+
+            SetStatus("Opening mod", MaterialIconKind.ArrowDecisionAuto);
+
+            await Task.Run(() => SetModContext(result));
+            ToolDock.SetActive("ModFiles");
+
+            SetStatus();
         }
 
-        public void OpenModActor()
+        public async void SaveActor()
         {
-            // if (mod is in context)
-            //    open in-app actor browser (from mod) without browse dialog
-            // else 
-            //    open folder browser for content
-            //    open in-app actor browser (from mod) and save mod context (prompt for context set unless settings deny it)
-        }
+            if (CurrentActor == null) {
+                return;
+            }
 
-        public void SaveActor()
-        {
-            // if (mod is in context)
-            //    save without browse dialog (prompt unless settings deny it)
-            // else 
-            //    open folder browser for content
-            //    save mod and set mod context (prompt for context set unless settings deny it)
+            string? output = null;
+
+            if (ModContext != null) {
+                var result = await View.ShowMessageBox($"A mod is open in the current session. Would you like to save to '{ModContext}'?", "Notice", MessageBoxButtons.YesNoCancel);
+
+                if (result == MessageBoxResult.Cancel) {
+                    return;
+                }
+                else if (result == MessageBoxResult.Yes) {
+                    output = ModContext;
+                }
+            }
+            
+            if (output == null) {
+                var result = await new OpenFolderDialog() {
+                    Title = $"Select a mod 'content' folder"
+                }.ShowAsync(View);
+
+                if (result == null)
+                    return;
+
+                if ((await View.ShowMessageBox($"Open this mod for future saving?", "Notice", MessageBoxButtons.YesNo)) == MessageBoxResult.Yes) {
+                    SetModContext(result);
+                }
+
+                output = result;
+            }
+
+            SetStatus("Saving actor", MaterialIconKind.ContentSaveMoveOutline);
+
+            await Task.Run(() => CurrentActor.Actor.Write(output!));
+
+            SetStatus();
         }
 
         public void Quit()
@@ -50,7 +112,7 @@ namespace BotwActorTool.GUI.ViewModels
         // 
         // Tools
 
-        public void Temp_PlaceHolder() => Debug.WriteLine("A placeholder action was executed!");
+        public void Temp_PlaceHolder() => View.ShowMessageBox("A placeholder action was executed!", "Notice");
         public void Settings() => SettingsView = new(View);
 
 
@@ -62,9 +124,19 @@ namespace BotwActorTool.GUI.ViewModels
             // open help wiki / help messgae (?)
         }
 
-        public void Credits()
+        public async void Credits()
         {
-            // show credits message
+            await View.ShowMessageBox(File.ReadAllText("./Assets/Credits.md"), "Credits", formatting: Formatting.Markdown);
+        }
+
+
+        //
+        // Helpers
+
+        public void SetModContext(string folder)
+        {
+            ModContext = folder;
+            (ToolDock.Get("ModFiles") as BrowserViewModel)!.SetRoot(folder, true);
         }
     }
 }
