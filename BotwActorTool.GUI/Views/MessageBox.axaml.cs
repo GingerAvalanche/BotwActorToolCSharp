6f5552d0ffacf5194c5f664a7990109012637506
ArchLeaders <archparliament@gmail.com>
2022-07-11 14:46:06 -0700

    Support markdown syntax in message boxes

--- BotwActorTool.GUI/Views/MessageBox.axaml.cs
+++ BotwActorTool.GUI/Views/MessageBox.axaml.cs
@@ -4,27 +4,43 @@ using Avalonia.Markup.Xaml;
 using Avalonia.Layout;
 using System.Threading.Tasks;
 using BotwActorTool.GUI.Extensions;
+using Markdown.Avalonia;
 
 namespace BotwActorTool.GUI.Views
 {
     public partial class MessageBox : Window
     {
         public MessageBox() => AvaloniaXamlLoader.Load(this);
-        public MessageBox(string title, string text)
+        public MessageBox(string title, string text, Formatting formatting)
         {
             AvaloniaXamlLoader.Load(this);
 
-            this.FindControl<TextBlock>("Text").Text = text;
+            var tb = this.FindControl<TextBlock>("Text");
+            if (formatting == Formatting.Markdown) {
+                tb.IsVisible = false;
+
+                var mdViewer = this.FindControl<MarkdownScrollViewer>("Markdown");
+                mdViewer.IsVisible = true;
+                mdViewer.Markdown = text;
+            }
+            else {
+                tb.Text = text;
+            }
+
             this.FindControl<TextBlock>("TitleBox").Text = title;
             this.FindControl<Button>("Copy").Click += async (_, _) => {
-                if (Application.Current != null && Application.Current.Clipboard != null)
-                    await Application.Current.Clipboard.SetTextAsync($"**{title}**\n```\n{text}\n```");
+                if (formatting == Formatting.Markdown) {
+                    await Application.Current!.Clipboard!.SetTextAsync($"**{title}**\n\n{text}");
+                    return;
+                }
+
+                await Application.Current!.Clipboard!.SetTextAsync($"**{title}**\n```\n{text}\n```");
             };
         }
 
-        public static Task<MessageBoxResult> Show(string text, string title = "Notice", MessageBoxButtons buttons = MessageBoxButtons.Ok, Window? parent = null)
+        public static Task<MessageBoxResult> Show(string text, string title = "Notice", MessageBoxButtons buttons = MessageBoxButtons.Ok, Window? parent = null, Formatting formatting = Formatting.None)
         {
-            MessageBox msgbox = new(title, text);
+            MessageBox msgbox = new(title, text, formatting);
             var res = MessageBoxResult.Ok;
 
             var buttonPanel = msgbox.FindControl<StackPanel>("Buttons");
@@ -35,13 +51,15 @@ namespace BotwActorTool.GUI.Views
                 msgbox.Close();
             };
 
-            void AddBtn(string caption, MessageBoxResult r, bool def = false)
+            void AddBtn(string caption, MessageBoxResult r, bool def = false, int mode = 0)
             {
                 var btn = new Button {
                     Content = caption,
                     HorizontalContentAlignment = HorizontalAlignment.Center,
-                    Margin = new Thickness(10,0,0,0),
-                    MinWidth = 67
+                    Margin = new Thickness(10, 0, 0, 0),
+                    MinWidth = 67,
+                    IsDefault = mode == 1,
+                    IsCancel = mode == 2
                 };
 
                 btn.Click += (_, __) => {
@@ -54,15 +72,15 @@ namespace BotwActorTool.GUI.Views
             }
 
             if (buttons == MessageBoxButtons.Ok || buttons == MessageBoxButtons.OkCancel)
-                AddBtn("Ok", MessageBoxResult.Ok, true);
+                AddBtn("Ok", MessageBoxResult.Ok, true, 1);
 
             if (buttons == MessageBoxButtons.YesNo || buttons == MessageBoxButtons.YesNoCancel) {
-                AddBtn("Yes", MessageBoxResult.Yes);
+                AddBtn("Yes", MessageBoxResult.Yes, mode: 1);
                 AddBtn("No", MessageBoxResult.No, true);
             }
 
             if (buttons == MessageBoxButtons.OkCancel || buttons == MessageBoxButtons.YesNoCancel)
-                AddBtn("Cancel", MessageBoxResult.Cancel, true);
+                AddBtn("Cancel", MessageBoxResult.Cancel, true, 2);
 
 
             var tcs = new TaskCompletionSource<MessageBoxResult>();
