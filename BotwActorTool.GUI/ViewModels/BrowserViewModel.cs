7d5ee3456fede6a84688234cba7c52e62677736d
ArchLeaders <archparliament@gmail.com>
2022-07-10 21:45:15 -0700

    Refactor source and user query

--- BotwActorTool.GUI/ViewModels/BrowserViewModel.cs
+++ BotwActorTool.GUI/ViewModels/BrowserViewModel.cs
@@ -2,7 +2,9 @@
 using Avalonia.Input;
 using Avalonia.Interactivity;
 using BotwActorTool.GUI.Extensions;
+using BotwActorTool.Lib;
 using Dock.Model.ReactiveUI.Controls;
+using Nintendo.Byml;
 using ReactiveUI;
 using System;
 using System.Collections.Generic;
@@ -11,38 +13,60 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Yaz0Library;
 
 namespace BotwActorTool.GUI.ViewModels
 {
-    public delegate void OnSelectionExecute(string field);
+    public delegate void OnSelectionExecute(string field, string modRoot);
     public class BrowserViewModel : Tool
     {
-        private readonly List<string> baseRoot = new();
-        public BrowserViewModel(object obj, OnSelectionExecute _onSelectionExecuted)
+        private readonly Dictionary<string, Dictionary<string, string>> baseRoot = new();
+        public BrowserViewModel(OnSelectionExecute _onSelectionExecuted, object? obj = null)
         {
             onSelectionExecuted = _onSelectionExecuted;
 
-            if (obj is string str) {
-                foreach (var file in Directory.GetFiles(str)) {
-                    var fname = Path.GetFileNameWithoutExtension(file);
-                    baseRoot.Add(fname);
+            if (obj == null) {
+
+                baseRoot["Normal"] = new();
+                baseRoot["Far"] = new();
+
+                BymlFile actorInfo = new(Yaz0.DecompressFast(Util.GetFileAnywhere(Config.UpdateDir, "/Actor/ActorInfo.product.sbyml")));
+
+                foreach (var actor in actorInfo.RootNode.Hash["Actors"].Array) {
+                    if (actor.Hash["name"].String is string name) {
+
+                        string desc = string.Join("\n",
+                            "Name: " + "WIP",
+                            "Bfres: " + (actor.Hash.ContainsKey("bfres") ? actor.Hash["bfres"].String : "???"),
+                            "Model: " + (actor.Hash.ContainsKey("mainModel") ? actor.Hash["mainModel"].String : "???"),
+                            "Profile: " + (actor.Hash.ContainsKey("profile") ? actor.Hash["profile"].String : "???")
+                        );
+
+                        if (name.EndsWith("_Far")) {
+                            baseRoot["Far"][name] = desc;
+                        }
+                        else {
+                            baseRoot["Normal"][name] = desc;
+                        }
+                    }
                 }
             }
             else if (obj is List<string> list) {
-                baseRoot = new(list);
+                // baseRoot = new(list);
             }
 
-            Root = baseRoot;
+            BrowseMode = BrowsingModes.First();
+            Root =new(baseRoot[BrowseMode]);
         }
 
-        private List<string> root = new();
-        public List<string> Root {
+        private SortedDictionary<string, string> root = new();
+        public SortedDictionary<string, string> Root {
             get => root;
             set => this.RaiseAndSetIfChanged(ref root, value);
         }
 
-        private string selected = "";
-        public string Selected {
+        private KeyValuePair<string, string> selected = new();
+        public KeyValuePair<string, string> Selected {
             get => selected;
             set => this.RaiseAndSetIfChanged(ref selected, value);
         }
@@ -54,30 +78,30 @@ namespace BotwActorTool.GUI.ViewModels
                 this.RaiseAndSetIfChanged(ref searchField, value);
 
                 if (string.IsNullOrEmpty(searchField)) {
-                    Root = baseRoot;
+                    Root = new(baseRoot[BrowseMode]);
                 }
                 else {
-                    Root = new(baseRoot.Where(s => s.ToLower().Contains(searchField.ToLower())));
+                    Root = new(baseRoot[BrowseMode].Where(
+                        s => s.Key.ToLower().Contains(searchField.ToLower())
+                    ).OrderBy(
+                        entry => entry.Value).ToDictionary(entry => entry.Key, entry => entry.Value)
+                    );
                 }
             }
         }
 
+        public List<string> BrowsingModes => baseRoot.Keys.ToList();
+
         private string browseMode = "Vanilla";
         public string BrowseMode {
             get => browseMode;
-            set => this.RaiseAndSetIfChanged(ref browseMode, value);
-        }
-
-        private List<string> browsingModes = new() {
-            "Vanilla",
-            "TitleBG"
-        };
-        public List<string> BrowsingModes {
-            get => browsingModes;
-            set => this.RaiseAndSetIfChanged(ref browsingModes, value);
+            set {
+                this.RaiseAndSetIfChanged(ref browseMode, value);
+                SearchField = searchField;
+            }
         }
 
         private readonly OnSelectionExecute onSelectionExecuted;
-        public void ExecuteSelectionChanged(object _) => onSelectionExecuted(Selected);
+        public void ExecuteSelectionChanged(object _) => onSelectionExecuted(Selected.Key, BrowseMode);
     }
 }
