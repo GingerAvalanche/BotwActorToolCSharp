2ff215b9b488c33e9c7e3fe9b88f3d787ed35941
ArchLeaders <archparliament@gmail.com>
2022-08-17 15:27:19 -0700

    YAML editor control

---
+++ BotwActorTool.GUI/Views/Editors/YamlEditor.axaml.cs
@@ -0,0 +1,57 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using AvaloniaEdit;
+using AvaloniaEdit.Document;
+using AvaloniaEdit.TextMate;
+using BotwActorTool.GUI.Controls;
+using BotwActorTool.GUI.ViewModels;
+using System;
+using System.Text;
+using TextMateSharp.Grammars;
+
+namespace BotwActorTool.GUI.Views.Editors
+{
+    public partial class YamlEditor : ActorEditor
+    {
+        public YamlEditor()
+        {
+            AvaloniaXamlLoader.Load(this);
+
+            base.editor = this.FindControl<TextEditor>("editor");
+            base.editor.Background = Brushes.Transparent;
+            base.editor.ShowLineNumbers = true;
+            base.editor.TextArea.Background = Background;
+            base.editor.Options.ShowBoxForControlCharacters = true;
+            base.editor.TextArea.IndentationStrategy = new AvaloniaEdit.Indentation.CSharp.CSharpIndentationStrategy(base.editor.Options);
+            base.editor.Encoding = Encoding.UTF8;
+            base.editor.Document = new TextDocument("404 not found");
+
+            RegistryOptions registryOptions = Config.IsDarkTheme ? new(ThemeName.DarkPlus) : new(ThemeName.LightPlus);
+            TextMate.Installation textMateInstallation = ((TextEditor)base.editor).InstallTextMate(registryOptions);
+            Language csharpLanguage = registryOptions.GetLanguageByExtension(".yml");
+            textMateInstallation.SetGrammar(registryOptions.GetScopeByLanguageId(csharpLanguage.Id));
+        }
+
+        public async void SetEditorMode(bool fast)
+        {
+            try {
+                if (fast) {
+                    RegistryOptions registryOptions = Config.IsDarkTheme ? new(ThemeName.DarkPlus) : new(ThemeName.LightPlus);
+                    TextMate.Installation textMateInstallation = ((TextEditor)base.editor!).InstallTextMate(registryOptions);
+                    textMateInstallation.SetGrammar(null);
+                }
+                else {
+                    RegistryOptions registryOptions = Config.IsDarkTheme ? new(ThemeName.DarkPlus) : new(ThemeName.LightPlus);
+                    TextMate.Installation textMateInstallation = ((TextEditor)base.editor!).InstallTextMate(registryOptions);
+                    Language csharpLanguage = registryOptions.GetLanguageByExtension(".yml");
+                    textMateInstallation.SetGrammar(registryOptions.GetScopeByLanguageId(csharpLanguage.Id));
+                }
+            }
+            catch (Exception ex) {
+                await MessageBox.Show($"{ex}", "Error Setting Editor Mode");
+            }
+        }
+    }
+}
