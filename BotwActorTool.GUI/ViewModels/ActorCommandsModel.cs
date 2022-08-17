f0a10575f212e586c53420c358b0a7db5f04e037
ArchLeaders <archparliament@gmail.com>
2022-08-17 15:19:27 -0700

    Editor ribbon commands

---
+++ BotwActorTool.GUI/ViewModels/ActorCommandsModel.cs
@@ -0,0 +1,102 @@
+﻿using AvaloniaEdit;
+using BotwActorTool.GUI.Views.Editors;
+using Dock.Model.ReactiveUI.Controls;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotwActorTool.GUI.ViewModels
+{
+    public partial class ActorViewModel : Document
+    {
+        public void Undo()
+        {
+            if (Editor.Value != null) {
+                (Editor.Value.editor as TextEditor)!.Undo();
+            }
+        }
+
+        public void Redo()
+        {
+            if (Editor.Value != null) {
+                (Editor.Value.editor as TextEditor)!.Redo();
+            }
+        }
+
+        public void Copy()
+        {
+            if (Editor.Value != null) {
+                (Editor.Value.editor as TextEditor)!.Copy();
+            }
+        }
+
+        public void Cut()
+        {
+            if (Editor.Value != null) {
+                (Editor.Value.editor as TextEditor)!.Cut();
+            }
+        }
+
+        public void Paste()
+        {
+            if (Editor.Value != null) {
+                (Editor.Value.editor as TextEditor)!.Paste();
+            }
+        }
+
+        public void SelectAll()
+        {
+            if (Editor.Value != null) {
+                (Editor.Value.editor as TextEditor)!.SelectAll();
+            }
+        }
+
+        public void Rename()
+        {
+
+        }
+
+        public void ToggleFastTextEditor()
+        {
+            IsFastMode = isFastMode == _isFastMode ? !isFastMode : isFastMode;
+            _isFastMode = isFastMode;
+
+            if (Editor.Value != null) {
+                ((YamlEditor)Editor.Value).SetEditorMode(IsFastMode);
+                var editor = Editor.Value.editor as TextEditor;
+                editor!.ScrollToLine(editor.Document.GetLocation(0).Line);
+            }
+        }
+
+        public void Save()
+        {
+            if (Editor.Value != null)
+                Editor.Value.Save(this);
+            App.SetStatus($"Saved {ActorFile}", Material.Icons.MaterialIconKind.ContentSaveCheck, false);
+        }
+
+        public void Cancel()
+        {
+            if (Editor.Value != null) {
+                Editors = AllEditors[actorFile];
+                Editor = Editors.First();
+            }
+        }
+
+        //
+        // Props
+        #region Expand
+
+        private bool _isFastMode = false;
+        private bool isFastMode = false;
+        public bool IsFastMode {
+            get => isFastMode;
+            set => this.RaiseAndSetIfChanged(ref isFastMode, value);
+        }
+
+        #endregion
+    }
+}
