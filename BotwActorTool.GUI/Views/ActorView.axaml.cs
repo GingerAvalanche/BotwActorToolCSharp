32fb8d3b1b03690afeb027756685049608ad027c
ArchLeaders <archparliament@gmail.com>
2022-08-17 15:22:04 -0700

    Ribbon buttons and sub editor binding

--- BotwActorTool.GUI/Views/ActorView.axaml.cs
+++ BotwActorTool.GUI/Views/ActorView.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
@@ -18,17 +19,25 @@ namespace BotwActorTool.GUI.Views
         {
             InitializeComponent();
 
-            // First of all you need to have a reference for your TextEditor for it to be used inside AvaloniaEdit.TextMate project.
-            var _textEditor = this.FindControl<TextEditor>("editor");
-            _textEditor.Background = Brushes.Transparent;
-            _textEditor.ShowLineNumbers = true;
-            _textEditor.TextArea.Background = Background;
-            _textEditor.Options.ShowBoxForControlCharacters = true;
-            _textEditor.TextArea.IndentationStrategy = new AvaloniaEdit.Indentation.CSharp.CSharpIndentationStrategy(_textEditor.Options);
-            RegistryOptions _registryOptions = new(ThemeName.DarkPlus);
-            TextMate.Installation _textMateInstallation = _textEditor.InstallTextMate(_registryOptions);
-            Language csharpLanguage = _registryOptions.GetLanguageByExtension(".yml");
-            _textMateInstallation.SetGrammar(_registryOptions.GetScopeByLanguageId(csharpLanguage.Id));
+            var ribbon = this.FindControl<StackPanel>("RIBBON");
+
+            foreach (Control _child in ribbon.Children) {
+                if (_child.Name != null) {
+                    var desc = _child.Name
+                        .Replace("__", " ")
+                        .Replace("b_0", "(")
+                        .Replace("b_1", ")")
+                        .Replace("n_l", "\n")
+                        .Replace("p_0", "+");
+
+                    if (_child is Button child_0) {
+                        ToolTip.SetTip(_child, $"{desc}\n{child_0.HotKey}");
+                    }
+                    else if (_child is ToggleButton child_1) {
+                        ToolTip.SetTip(_child, $"{desc}\n{child_1.HotKey}");
+                    }
+                }
+            }
         }
 
         private void InitializeComponent()
