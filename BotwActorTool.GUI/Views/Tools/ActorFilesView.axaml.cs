b720b4b7aa2f0f8ed259ba96d66a86217ba5ec87
ArchLeaders <archparliament@gmail.com>
2022-11-21 23:53:04 -0800

    Stage ActorFiles View/ViewModel

---
+++ BotwActorTool.GUI/Views/Tools/ActorFilesView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace BotwActorTool.GUI.Views.Tools
+{
+    public partial class ActorFilesView : UserControl
+    {
+        public ActorFilesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
