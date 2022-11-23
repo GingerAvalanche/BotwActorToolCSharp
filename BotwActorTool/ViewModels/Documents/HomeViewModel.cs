2006a809aaf37d532d4c9614311d9d0763184995
ArchLeaders <archparliament@gmail.com>
2022-11-23 01:48:11 -0800

    Create home page view/view model Impl drag/drop and version link function

---
+++ BotwActorTool/ViewModels/Documents/HomeViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Dock.Model.ReactiveUI.Controls;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace BotwActorTool.ViewModels.Documents
+{
+    public class HomeViewModel : Document
+    {
+        public HomeViewModel()
+        {
+            Title = "Home";
+            CanClose = false;
+            CanFloat = false;
+            CanPin = false;
+        }
+
+        public void VersionLink()
+        {
+            ProcessStartInfo info = new() {
+                CreateNoWindow = true,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+                info.FileName = "cmd";
+                info.Arguments = $"/c start https://github.com/GingerAvalanche/BotwActorToolCSharp/releases/{Meta.Version}";
+                Process.Start(info);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
+                info.FileName = "xdg-open";
+                info.Arguments = $"https://github.com/GingerAvalanche/BotwActorToolCSharp/releases/{Meta.Version}";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
+                info.FileName = "open";
+                info.Arguments = $"https://github.com/GingerAvalanche/BotwActorToolCSharp/releases/{Meta.Version}";
+            }
+            else {
+                throw new Exception("Unknown operating system");
+            }
+
+            Process.Start(info);
+        }
+    }
+}
---
+++ BotwActorTool/Views/Documents/HomeView.axaml.cs
@@ -0,0 +1,31 @@
+using Avalonia.Controls;
+using Avalonia.Generics.Dialogs;
+using Avalonia.Input;
+using System.IO;
+using System.Linq;
+
+namespace BotwActorTool.Views.Documents
+{
+    public partial class HomeView : UserControl
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+            DragRegion.AddHandler(DragDrop.DropEvent, DragDropEvent);
+        }
+
+        public async void DragDropEvent(object? sender, DragEventArgs e)
+        {
+            string? file = e.Data.GetFileNames()?.FirstOrDefault();
+
+            if (file != null && file.EndsWith(".sbactorpack")) {
+                if (File.Exists($"{file}/../../ActorInfo.product.sbyml")) {
+                    await MessageBox.ShowDialog($"Could not find ActorInfo relative to Actor {Path.GetFileNameWithoutExtension(file)}!", "Error");
+                    return;
+                }
+
+                // Open Actor
+            }
+        }
+    }
+}
