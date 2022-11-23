c4cfc1fe7f9c4a43dd6c6584afe5804ee8c81b73
ArchLeaders <archparliament@gmail.com>
2022-11-23 01:43:56 -0800

    Remove C# suffix in footer string

--- BotwActorTool/Meta.cs
+++ BotwActorTool/Meta.cs
@@ -6,7 +6,7 @@ namespace BotwActorTool
     {
         public static string Name { get; } = "Botw Actor Tool";
         public static string? Version { get; } = typeof(Meta).Assembly.GetName().Version?.ToString(3);
-        public static string Footer { get; } = $"{Name} (C#) - v{Version}";
+        public static string Footer { get; } = $"{Name} - v{Version}";
         public static string BaseUrl { get; } = "https://raw.githubusercontent.com/GingerAvalanche/BotwActorToolCSharp/master";
         public static bool IsWindows { get; } = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
     }
