7d4e27b160254b2205712dfa5905eaa9ccb17038
ArchLeaders <archparliament@gmail.com>
2023-01-14 10:58:11 -0800

    Create home page view/view model

---
+++ BotwActorTool/Views/Documents/HomeView.axaml.cs
@@ -0,0 +1,29 @@
+using Avalonia.Controls;
+using Avalonia.Generics.Dialogs;
+using Avalonia.Input;
+
+namespace BotwActorTool.Views.Documents
+{
+    public partial class HomeView : UserControl
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+            DragRegion.AddHandler(DragDrop.DropEvent, DragDropEvent);
+        }
+
+        public async void DragDropEvent(object? sender, DragEventArgs e)
+        {
+            string? file = e.Data.GetFileNames()?.FirstOrDefault();
+
+            if (file != null && file.EndsWith(".sbactorpack")) {
+                if (!File.Exists($"{file}/../../ActorInfo.product.sbyml")) {
+                    await MessageBox.ShowDialog($"Could not find an ActorInfo file relative to the imported Actor ({Path.GetFileNameWithoutExtension(file)})", "Error");
+                    return;
+                }
+
+                // Open Actor
+            }
+        }
+    }
+}
