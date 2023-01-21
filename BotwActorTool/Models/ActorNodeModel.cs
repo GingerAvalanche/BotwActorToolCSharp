0b3e2daa1f09fe96e099f1f789eb694f9764e054
ArchLeaders <archparliament@gmail.com>
2023-01-21 12:51:31 -0800

    Rename TreeNodeModel

---
+++ BotwActorTool/Models/ActorNodeModel.cs
@@ -0,0 +1,23 @@
+﻿using Nintendo.Byml;
+
+namespace BotwActorTool.Models
+{
+    public class ActorNodeModel : ReactiveObject
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public BymlNode Meta { get; set; } = new("null");
+
+        private bool _visible = true;
+        public bool Visible {
+            get => _visible;
+            set => this.RaiseAndSetIfChanged(ref _visible, value);
+        }
+
+        public ActorNodeModel(string name, string tooltip = "")
+        {
+            Name = name;
+            Description = tooltip;
+        }
+    }
+}
