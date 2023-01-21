18153995e6574ee823a14ca66df326cc58d3bf78
ArchLeaders <archparliament@gmail.com>
2023-01-21 12:52:14 -0800

    Create ModNodeModel

---
+++ BotwActorTool/Models/ModNodeModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+
+namespace BotwActorTool.Models
+{
+    public class ModNodeModel : ReactiveObject
+    {
+        public ModNodeModel(string name, string? description)
+        {
+            Name = name;
+            Description = description;
+
+            // Populate Actors from mod source (backend)
+            Actors = new();
+        }
+
+        public string Name { get; set; }
+        public string? Description { get; set; }
+
+        private bool _visible = true;
+        public bool Visible {
+            get => _visible;
+            set => this.RaiseAndSetIfChanged(ref _visible, value);
+        }
+
+        private ObservableCollection<ActorNodeModel> _actors;
+        public ObservableCollection<ActorNodeModel> Actors {
+            get => _actors;
+            set => this.RaiseAndSetIfChanged(ref _actors, value);
+        }
+
+        public bool Search(string str)
+        {
+            bool any = false;
+            foreach (var actor in Actors) {
+                actor.Visible = string.IsNullOrEmpty(str) || str.Contains(actor.Name.ToLower()) || actor.Name.ToLower().Contains(str);
+                any = actor.Visible ? true : any;
+            }
+
+            return any;
+        }
+    }
+}
